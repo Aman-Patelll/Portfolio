5f35da21cc0d17f5210461995514c8a8e263bb11
Aman Patel <Aman369patel@gmail.com>
2024-01-03 09:34:07 +0530

    Add Files

---
+++ src/data/constants.js
@@ -0,0 +1,285 @@
+export const Bio = {
+	name: "Aman Patel",
+	roles: [
+		"Full Stack Developer",
+		// "Android Developer",
+		// "UI/UX Designer",
+		"Programmer",
+	],
+	description:
+		"I am highly skilled MERN Stack and React-native Developer with a passion for building scalable and user-friendly web & mobile application",
+	github: "https://github.com/Aman-Patelll",
+	resume:
+		"https://drive.google.com/file/d/1O1DqhLvU8HK200kdIB_tiuEgrfMIgKjA/view?usp=sharing",
+	linkedin: "https://www.linkedin.com/in/aman-patelll/",
+	// twitter: "https://twitter.com/RishavChanda",
+	insta: "https://www.instagram.com/amanpatel009/",
+	facebook: "https://www.facebook.com/profile.php?id=100037160026790",
+};
+
+export const skills = [
+	{
+		title: "Frontend",
+		skills: [
+			{
+				name: "HTML",
+				image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+				progress: 95,
+			},
+			{
+				name: "CSS",
+				image:
+					"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+				progress: 92,
+			},
+			{
+				name: "JavaScript",
+				image:
+					"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+				progress: 95,
+			},
+			{
+				name: "React Js",
+				image:
+					"https://cdn.iconscout.com/icon/free/png-256/free-react-1-282599.png?f=webp",
+				progress: 88,
+			},
+			{
+				name: "Chakra UI",
+				image:
+					"https://ph-files.imgix.net/561a8bda-142f-4c44-b3ba-1831e564218d.png?auto=format",
+				progress: 95,
+			},
+			{
+				name: "Material UI",
+				image:
+					"https://static-00.iconduck.com/assets.00/material-ui-icon-2048x1626-on580ia9.png",
+				progress: 85,
+			},
+			{
+				name: "Bootstrap",
+				image:
+					"https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+				progress: 87,
+			},
+			{
+				name: "Redux",
+				image:
+					"https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+				progress: 80,
+			},
+		],
+	},
+	{
+		title: "Backend",
+		skills: [
+			{
+				name: "Node Js",
+				image: "https://nodejs.org/static/images/logo.svg",
+				progress: 92,
+			},
+			{
+				name: "Express Js",
+				image: "https://img.icons8.com/office/480w/express-js.png",
+				progress: 91,
+			},
+			{
+				name: "MySQL",
+				image:
+					"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBBy0d4m3a35llK5EjSzdbUt2v4DiogO5beg&usqp=CAU",
+				progress: 88,
+			},
+			{
+				name: "MongoDB",
+				image: "https://asset.brandfetch.io/idYHMwWF60/idGhyEM0wZ.png",
+				progress: 88,
+			},
+		],
+	},
+	{
+		title: "Programming",
+		skills: [
+			{
+				name: "C",
+				image:
+					"https://i.pinimg.com/originals/6e/46/e7/6e46e7dbe2bb73dacc055e5dbd85c3ad.png",
+				progress: 96,
+			},
+			{
+				name: "C++",
+				image:
+					"https://w7.pngwing.com/pngs/46/626/png-transparent-c-logo-the-c-programming-language-computer-icons-computer-programming-source-code-programming-miscellaneous-template-blue.png",
+				progress: 94,
+			},
+			{
+				name: "Java",
+				image:
+					"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzjiYG5vmjT7tjLa0QXiUsmbu1ThFrwV1Tf-sfKrX9fHgmJQMTbBzxdr3qkM_SS1SjdNk&usqp=CAU",
+				progress: 92,
+			},
+			{
+				name: "Python",
+				image:
+					"https://storage.needpix.com/rsynced_images/language-2024210_1280.png",
+				progress: 90,
+			},
+		],
+	},
+	{
+		title: "Others",
+		skills: [
+			{
+				name: "Git",
+				image: "https://iconape.com/wp-content/png_logo_vector/git-icon.png",
+				progress: 90,
+			},
+			{
+				name: "GitHub",
+				image:
+					"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+				progress: 90,
+			},
+			{
+				name: "VS Code",
+				image:
+					"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+				progress: 96,
+			},
+			{
+				name: "Postman",
+				image:
+					"https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+				progress: 95,
+			},
+		],
+	},
+];
+
+export const experiences = [
+	{
+		id: 0,
+		img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+		role: "DevOps & Fullstack Engineering Intern",
+		company: "Flipr Inovations Pvt. Ltd.",
+		date: "Aug 2023 - July 2023",
+		desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+		skills: [
+			"Docker",
+			"Terraform",
+			"AWS",
+			"EC2",
+			"Portainer",
+			"Nginx",
+			"JavaScript",
+			"TypeScript",
+			"Node Js",
+			" Next Js",
+		],
+		doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+	},
+];
+
+export const education = [
+	{
+		id: 0,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFXWwxYCfOh9i9fv70BztRLXtncv4Hs3r0ASSVDrBkBA&s",
+		school:
+			"Shri Vaishnav Vidhyapeeth Vishawavidhyalaya, Indore, Madhya Pradesh",
+		date: "Oct 2021 - Sep 2025",
+		grade: "8.5 CGPA",
+		desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Shri Vaishnav Vidhyapeeth Vishawavidhyalaya, Indore, Madhya Pradesh. I have completed 5 semesters and have a CGPA of 8.5. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+		degree:
+			"Bachelor of Technology - BTech, Computer Science and Engineering with Specilization in Information and Cyber Security",
+	},
+	{
+		id: 1,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0n87wo1hVi_GdsHcNMjRZ_6my1usQkLgTEL4H6Qqp8g&s",
+		school:
+			"Eminence Public Higher Secondary School, Tonk-Khurd, Dewas, Madhya Pradesh",
+		date: "Jun 2020 - May 2021",
+		grade: "91.2%",
+		desc: "I completed my class 12 high school education at Eminence Public Higher Secondary School, Tonk-Khurd, Dewas, where I studied Maths",
+		degree: "XII",
+	},
+	{
+		id: 2,
+		img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0n87wo1hVi_GdsHcNMjRZ_6my1usQkLgTEL4H6Qqp8g&s",
+		school:
+			"Eminence Public Higher Secondary School, Tonk-Khurd, Dewas, Madhya Pradesh",
+		date: "Jun 2018 - May 2019",
+		grade: "88.4%",
+		desc: "I completed my class 10 education at Eminence Public Higher Secondary School, Tonk-Khurd, Dewas",
+		degree: "X-",
+	},
+];
+
+export const projects = [
+	{
+		id: 9,
+		title: "Course Bundler E-Learning Platform",
+		date: "Jul 2023 - Aug 2023",
+		description:
+			"Course Bundler a E-learning platform based on Subscription Model. • Key Features are buy Subscription, cancel Subscription, browse all courses, request for course, contact us, registration, change photo & profile, forget password, add to playlist, remove from playlist etc. • Admin DashBord where Admin can create & delete course, add & remove lectures into a course, see all user information, change role, remove user, get all courses & there lectures etc. • ONLY SUBSCRIBERS ACCESS COURSES",
+		image:
+			"https://lh3.googleusercontent.com/pw/ABLVV84pfeTw4iOUOlax_0wfVASuEedIBj4TpuKlFQ9M3EkEnJt9BIYofFpo70uVmyseBp9w-5hTL9I3JojJIaP6uCz3nNTvkueciaBrZS-6zQpM6B4wd0Iuoj2c4p0dnuv6tcpbkd5clFQrzIcpJ3xMv69-dJz_cp4aV-HU_NpqlulZlyD0c3PMBdqKvq-nTXepxhmECTbtfRceAPM5ArRK9InOEuVjJgOyHYW9owGtAtoBMTUEurbcDVp9Kv_U2vzq0UG5kh3H1qRfVEBhJ57j8miv9SXy0xFMxLdDNdwQ4MvVznkejEZ1Hd6MYeR94yCsQz8aEa2jk1U-qSgh-3eEJQDVKYs4xlf9fhOmQR1Nc8eNcuwaNny2AmpSzhjQWTIzG4gKwRQN3bdlWZuC7-TsKOxlemFiaMR4IqQpKUEWvc4GKsWPadNisML5z66ctX9rjVA9NW7w3k3q28he9_gZXf__Ex2DfggBMsfyVLVoyusgKoZ6TKTlyJboqlLllhNHpn9NfbUEidmN62Aoj4xlLYpcfW-uOd_ePFbtLp2fA48-m8b37JU0aDZh64H0OoHGS0SFwzIhNXKET3Ctl85FlH4O6mfv-mGrvvsBzERWvHyO4vjeSsN-XyWZvuwp6r5Hj7r36U1sZoQQMDU715GAWut9EMXXzsTt3Hx2hCMzRl04cgncGVkZ7lxd0kKaYcbiuIDGnYsShx6OZo-ci2DOWU06dAenYTIXDCfFQUAOgvp8Exk3DI7MWu6CZiyf-ywCESvm2QZ3ruzDidBC-sSTzUOcoSR_JX1eWmol5OTjZUTlOnBOd2-b64tTmQxUXC0SGv6G7zhwWFTrDfG0AbZC6E6l8e2z0HQzndZvWYK3lsshDEoiRcxAV-hqMJMOh7kqyLSs3A=w864-h864-s-no-gm?authuser=0",
+		tags: [
+			"JavaScript",
+			"React",
+			"React-Redux",
+			"Node Js",
+			"Express Js",
+			"MongoDB",
+			"Cloudinary",
+			"Razorpay",
+			"MongoDb",
+			"Chakra UI",
+			"Chart Js",
+			"Nodemailer",
+		],
+		category: "web app",
+		github: "https://github.com/Aman-Patelll/courseBundler",
+		webapp: "https://coursebundler-dun.vercel.app/",
+	},
+	{
+		id: 0,
+		title: "Lets Chat Full Stack Chatting Application",
+		date: "Apr 2023 - May 2023",
+		description:
+			"Let’s Chat a Full Stack Real Time Chatting App Using React JS, Node JS, Express JS, MongoDB, Chakra-ui, socket.io & JavaScript. • Key Features are one to one chat, group chat, authentication, real time chatting with typing indicator, search users, create group chats, add & remove user from group, notification, view other user profile",
+		image:
+			"https://lh3.googleusercontent.com/pw/ABLVV870CuXlOA1MLKhJlIShdai0mCbSih3d0ES3HLOBau1zKRtJqvOqLQlguD47VHseJtZbbgSHCTuL7zCPDw8_LDTjsmKwunQ8bUAGUsVmu81OdkKD7A6jdbBqs2Dv54cg5GVBN9XhHY5kR7H2eAbJFqUGdg9XDPpEJDfu77VguhUqJI6PL-lNGpHOL74lv39aWDEMXXOXrSPNoTT_RkaHWQBFyyyN-fpxQrHEBPjn24GmA_rcvWFbRttz4oucxwNbc_8q2XJ7WWQlqVWLa9_nCq0_NDNW5cN0-u_cv1YLC70HhZfUPkLQv9_ZbOVPPp4vGfAUG_jKqSVQyn4y8C0Xi-aGQwZCRl1XQ-tvckKoqbzuxpZICk4LJ1VG7Ltoz7H7AM3z2H5saqz5Vxhm8M-ampBknTy_XcyP8Ri0pb60VyLEnI7mvYveL98t2GIyBiPkZLDqlsL57diHhLD9Bgc4_dBoH7adIOZG-ZaeN0A1nGqrJLv7HBVjfqDk6aP4tfRigUOFQ5uElgRgHbzeysoYWzq8D2mKMH4RpVg1I8Toh8LUzQFOH1pSGA1jl6VkFM9_-0c3Z5r3O9vuNsQFjcqrZ12rv3UelgmmRpPe-qqZ4do3lFut13OJiG4B6gIWoNPsluIrxHFSiXL41hkDHEes91iDSfM6jeuVuzI1zKbj8f-rg3juA30lCMR0_bTD8oVp4SXMQbcN2yaWutnfG_fAFSw6wc_GgF0qwFURQPdr2oEniDan-XqsCZHrYaCpoHTi-Z5WOggSKynMce1SsRc4USmbaVMCaylPxqbaW0Qr7OciNVFZjBV-gM_Q3YXnB0HvgF9L1zBrOZbEicOsvm7QN0rDBg_sFdt18aILARPQvO4aI1AfTNNMsNMvtDDkhIHHxkFh_A=w864-h864-s-no-gm?authuser=0",
+		tags: [
+			"React JS",
+			"Node JS",
+			"Express JS",
+			"MongoDB",
+			"Chakra-ui",
+			"socket.io",
+			"JavaScript",
+		],
+		category: "web app",
+		github: "https://github.com/Aman-Patelll/chat-app",
+		webapp: "https://lets-chat-7ce3.onrender.com",
+	},
+	{
+		id: 1,
+		title: "Responsive Social Media Website With Theme Customization",
+		date: "Dec 2022 - Jan 2023",
+		description:
+			" Website Element: Navigation bar, sidebar and notifications popup, stories, create post section, feeds or posts, messages and friend requests. ",
+		image:
+			"https://lh3.googleusercontent.com/pw/ABLVV853o_3OXMVENys6BCUVwELAgp2kK4u0efPUKzW4kHHnKcwvyW_rjZYWLP6hHUftel1Hiq6ZAXGi-pJFQXAGVD_FfLq6rpOPSr1T_K4-GrhupiTBNB27sWG7-_x9ol4MmGftgeELwD8sdHJ6Hg8AYnI_1UjNgZwiJOD5I4F-auKIHUCKGfyoGGxlxY4vN5MtBn2wXitg1nqAwwup9kzzCum4MzyCF1RjYntQhcQKuoXVTtGuyaLkyfFGOowOqxmzuHXRmQGmEg3PMvfw7RlMcdwu-YUZ1eK3Yhgc_qVAwzQEzz2QQkOJyz1fQI_7oIIyKyd4pApLodoN6pbmC3aJxqlVNCm1Zlg7m-fiM7aas1b-DnoGp34XZOhR-xsffXwoe4T2UO4N3tqw805tziGnFo75XNEzmh5JJT37NazQ6-lxIqQ8VUROcYIfDlg9EjI6ipXqgbCupiHLfQ5bDLito8oixlx3DCiBPM2wguZ3eKNpMFXVVn6qnVnzQXsBUNUDUjxm3dzbN99MuHSy0jqrHARO2MN6qqvGWJ8BZBDbIA8cSZg3iL3zA8D1MoULUdz7EqIoRRI4ytQPocVbfBsEnJyHPDYgGM_aIxRO5MzT9rmEIIIsBRsMDqRmeJgaexlpvtJ2lUCpRuZmP9ghhiPvuujDwiG10K1NwiJZDgjgfUberQjHVQs7M8sQlbe6v2DbzEq2nnXBzfufhnAGS-pI5NwVqAnEP95xCa34mf_m-sVeG6MZ_N-HVHmFhW4HP8rDAceXrlOSk6H6fGURs49w99hVqbgJ2H56C13kBmTrexX6WHtJlH5Uxz1L1hir1oweKucX4hYzOG57A7j-yFu5w-IGBnvf_R51sD0BTsdd1np2euXyu-eZ3l9bW8FqUWLrhpkPcA=w864-h864-s-no-gm?authuser=0",
+		tags: ["HTML", "CSS", "JavaScript"],
+		category: "web app",
+		github: "https://github.com/Aman-Patelll/Responsive-Social-Media",
+		webapp: "https://responsive-social-media.onrender.com/",
+	},
+];
+
+export const TimeLineData = [
+	{ year: 2017, text: "Started my journey" },
+	{ year: 2018, text: "Worked as a freelance developer" },
+	{ year: 2019, text: "Founded JavaScript Mastery" },
+	{ year: 2020, text: "Shared my projects with the world" },
+	{ year: 2021, text: "Started my own platform" },
+];
